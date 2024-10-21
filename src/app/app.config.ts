2b63452e4b182f715c79d3be2b407eeee6110743
Oskar <actionman123@interia.pl>
2024-10-21 12:41:00 +0200

    firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"chat-app-2f30c","appId":"1:955038607258:web:c995ac0b8bf1c103b67c23","storageBucket":"chat-app-2f30c.appspot.com","apiKey":"AIzaSyCIG77J0UC1jD_3dNqXYQcqLFDmmn-033k","authDomain":"chat-app-2f30c.firebaseapp.com","messagingSenderId":"955038607258"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
